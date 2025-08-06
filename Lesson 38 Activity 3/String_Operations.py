b646f31769865bfe83c45d5ec1ed27a5ecf16e00
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-06 07:31:29 +0530

    Initial Commit

---
+++ Lesson 38 Activity 3/String_Operations.py
@@ -0,0 +1,14 @@
+first_name = "Codingal"
+last_name = "Education"
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+Hello = "Hello XXX. "
+print(Hello * 3)
+
+word = "Python"
+print("First character:", word[0])
+print("Last character:", word[-1])
+print("First three characters Sliced:", word[:3])
+print("Last three characters Sliced:", word[-3:])
+print("Length of Word:", len(word))

21560725b58eb8819a3931650c076285255361bb
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-06 09:34:06 +0530

    Initial Commit

---
+++ Lesson 40 Activity 3/NaturalNumber_Sum.py
@@ -0,0 +1,6 @@
+print(" Printing the sum of 1st 10 natural numbers")
+
+natural_number = list(range(1, 11))
+print("Natural Numbers:", natural_number)
+sum_of_natural_numbers = sum(natural_number)
+print("Sum of 1st 10 natural numbers:", sum_of_natural_numbers)

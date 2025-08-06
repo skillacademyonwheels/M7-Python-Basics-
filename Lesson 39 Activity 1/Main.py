c31cbc1273bd197348ecee103d1dcbf4357a637c
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-06 09:00:19 +0530

    2nd Commit

--- Lesson 39 Activity 1/Main.py
+++
@@ -1,4 +0,0 @@
-number = 98
-
-if number < 100:
-    print("The number is less than 100.\n")
---
+++ Lesson 39 Activity 1/OddEven.py
@@ -0,0 +1,9 @@
+print("This program checks if a number is odd or even.")
+num_input = int(input("Enter a number: "))
+
+if num_input % 2 == 0:
+    print(f"{num_input} is an even number.")
+
+else:
+    print(f"{num_input} is an odd number.")
+    

61b61db49375f49746f5203715ad1b9b0d3c29e1
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-06 07:36:49 +0530

    Initial Commit

---
+++ Lesson 38 Activity 4/Number_Swap.py
@@ -0,0 +1,18 @@
+# Taking input from user
+from re import X
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Swapping the numbers
+#num1, num2 = num2, num1
+temp = num1
+num1 = num2
+num2 = temp
+
+
+# Displaying the swapped numbers
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)

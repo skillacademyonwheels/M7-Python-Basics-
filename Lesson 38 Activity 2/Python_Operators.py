89c2d7e2c2b5388157941a63ca7c481bbd280a7d
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-06 07:24:26 +0530

    Initial Commit

---
+++ Lesson 38 Activity 2/Python_Operators.py
@@ -0,0 +1,19 @@
+num1 = 23
+num2 = 50
+
+print("Number 1:", num1)
+print("Number 2:", num2)
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+print("Is num1 equal to num2?", num1 == num2)
+print("Is num1 not equal to num2?", num1 != num2)
+print("Square of num1:", num1 ** 2)
+print("square root of num1:", num1 ** 0.5)
+
+result = num1/2 + num2**2 + 10
+print("Result of complex expression:", result)

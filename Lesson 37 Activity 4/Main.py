4934c22ceb267cc651fb43321f858665726b5f15
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-06 07:02:38 +0530

    Initial Commit

---
+++ Lesson 37 Activity 4/Main.py
@@ -0,0 +1,14 @@
+# Print a Sentence
+print("Hello, World! This is my first Python program.")
+
+# Print a Number
+print(42)
+
+# Next Line after print
+print("This is the next line after printing a number.\n")
+
+# Print all keywords in Python
+import keyword
+print("Python Keywords:")
+for kw in keyword.kwlist:
+    print(kw)
